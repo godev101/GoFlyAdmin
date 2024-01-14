2804829b4bf21ee55769da992b6e8c1201867b25
zhenyong.li <gocoder1987@163.com>
2024-01-15 04:58:05 +0800

    update log

--- bootstrap/router.go
+++ bootstrap/router.go
@@ -30,8 +30,8 @@ func RunServer() {
 	model.MyInit(1) //初始化数据
 	if global.App.Config.App.Env == "dev" {
 		fmt.Printf("\n %c[1;40;32m%s%c[0m\n", 0x1B, "在浏览器访问：​​http://127.0.0.1:"+global.App.Config.App.Port+"/common/install/index ​进行​安装​", 0x1B)
-		r.Run(":" + global.App.Config.App.Port)
 		global.App.Log.Info("启动端口：" + global.App.Config.App.Port)
+		r.Run(":" + global.App.Config.App.Port)
 	} else { //优雅-生产环境使用
 		//换一种启动方式
 		srv := &http.Server{
